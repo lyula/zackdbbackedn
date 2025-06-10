3d586b509deb76c797fe0c8c14acadce962c3b03
lyula <sacredlyula@gmail.com>
2025-06-10 10:22:26 +0300

    added created and updated at fields in my savedconnections collection schema

--- models/ConnectionString.js
+++ models/ConnectionString.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const ConnectionStringSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   connectionString: { type: String, required: true }
-}, { collection: 'savedconnections' });
+}, { 
+  collection: 'savedconnections',
+  timestamps: true
+});
 
 module.exports = mongoose.model('ConnectionString', ConnectionStringSchema);
