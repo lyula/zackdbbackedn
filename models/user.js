fb6963ce03efe1a4b06f5afc9db92928a326972f
lyula <sacredlyula@gmail.com>
2025-06-10 10:35:55 +0300

    added created and updated at timetamps to the users collection

--- models/user.js
+++ models/user.js
@@ -4,6 +4,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { timestamps: true }); // <-- Added this option to show created at and updated at timestamps
 
 module.exports = mongoose.model('User', userSchema);
